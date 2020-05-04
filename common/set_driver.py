91637f09fc6944441288e64bf586798616fb4657
lifangfang <13077313846@163.com>
2020-05-04 12:11:16 +0800

    第十一次提交： set_driver_py中增加取消弹窗的设置，和取消忘记密码的设置

--- common/set_driver.py
+++ common/set_driver.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 from common.config_utils import config
 from selenium.webdriver.chrome.options import Options
-import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def set_driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('disable-infobars')
+    prefs = {"": ""}
+    prefs["credentials_enable_service"] = False
+    prefs["profile.password_manager_enabled"] = False
+    chrome_options.add_experimental_option("prefs", prefs)  ##关掉密码弹窗
+    chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+    chrome_options.add_argument('lang=zh_CN.UTF-8')  # 设置默认编码为utf-8
+    chrome_options.add_experimental_option('useAutomationExtension', False)  # 取消chrome受自动控制提示
+    chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])  # 取消chrome受自动控制提示
+
     driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     return driver
