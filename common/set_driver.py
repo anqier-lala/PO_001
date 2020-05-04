04c3763808ca47cf941216241d324cf9379b4ac5
lifangfang <13077313846@163.com>
2020-05-04 11:37:25 +0800

    第十次提交： set_py中增加分布式方法

--- common/set_driver.py
+++ common/set_driver.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from common.config_utils import config
 from selenium.webdriver.chrome.options import Options
+import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def set_driver():
@@ -10,8 +12,11 @@ def set_driver():
     driver.maximize_window()
     return driver
 
-# def __get_remote_driver(self):  # selenium支持分布式 grid == > 配置（你自己的代码编写、配置）
-#     pass
+def __get_remote_driver(self):  # selenium支持分布式 grid == > 配置（你自己的代码编写、配置）
+    driver = webdriver.Remote(
+    command_executor='http://127.0.0.1:4444/wd/hub',
+    desired_capabilities=DesiredCapabilities.FIREFOX)
+
 
 if __name__ == '__main__':
     set_driver()
